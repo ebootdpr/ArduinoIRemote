def9fd63ec1a0d5224f6fba1707da84564bebc43
ebootdpr <36868128+ebootdpr@users.noreply.github.com>
2021-12-31 07:54:31 -0300

    Add files via upload

---
+++ Proyecto_Integrador_Bloque_h_ifs.ino
@@ -0,0 +1,185 @@
+/*
+  Realizamos un programa para leer los datos recibidos a traves de un control remoto 
+  y los vamos a colocar en una tabla para su posterior uso
+*/
+
+
+
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+
+/*INICIO VARIABLES
+
+  Para deteccion de errores usaremos el led integrado del pin 13.
+  La entrada será el pin 2 (donde arduino detectará una interrupción y va a leer el codigo recibido del control remoto
+  con ayuda de la librería)
+
+*/
+int LuzDelanteraIzquierda = 2;
+int LuzDelanteraDerecha= 4;
+int LuzTraseraIzquierda = 7;
+int LuzTraseraDerecha = 12;
+int ledTest = 13;
+int MemoryLastIf=0;
+double contador=0;
+/*
+   Seteo del receptor infrarojo
+*/
+int ReceptorIR = 8;                     // Declaramos que usaremos el pin 8 (asociado a la variable ReceptorIR) como entrada del control remoto. 
+IRrecv receptorIr(ReceptorIR);          // Creamos un objeto de la clase IRrecv llamado "receptorIr"
+
+decode_results codigoLeido;              //declaramos 2 variables de tipo decode_results llamadas "codigoLeido" y "codigoLeidoNuevo"
+decode_results codigoLeidoNuevo;
+
+
+//Definimos variables
+
+const int AIA = 3; 
+const int AIB = 11;
+const int BIA = 10; 
+const int BIB = 9;
+
+
+/*FIN VARIABLES*/
+
+void setup() {
+
+    //indicar los pines de salida
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(12, OUTPUT);
+  //apagar todos los leds
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(12, LOW);
+  /*
+     Seteo receptor infrarojo
+  */
+  Serial.begin(9600);
+  receptorIr.enableIRIn();     // Habilitamos al receptor para que comience a intentar detectar señales infrarrojas.
+
+  
+  pinMode(AIA, OUTPUT); 
+  pinMode(AIB, OUTPUT);
+  pinMode(BIA, OUTPUT);
+  pinMode(BIB, OUTPUT);
+ 
+
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 0);
+  analogWrite(BIA, 0);
+  analogWrite(BIB, 0);
+
+
+  pinMode(ledTest, OUTPUT);
+  digitalWrite(ledTest, LOW);
+
+  }
+
+
+void loop() {  
+  if (receptorIr.decode(&codigoLeidoNuevo)){receptorIr.resume();
+  Serial.println(codigoLeidoNuevo.value, HEX); 
+  contador++;
+  }
+
+  
+  if (contador < 1)
+  {
+    MemoryLastIf=0;
+    contador=0;
+    parar();
+  }
+  // Serial.println(codigoLeidoNuevo.value, HEX); 
+   codigoLeido = codigoLeidoNuevo;     
+
+      if (codigoLeidoNuevo.value == 0xFF9867 || MemoryLastIf == 1 ){  //Avanzar    
+        adelante();
+        MemoryLastIf == 1;
+         }                      
+      if (codigoLeidoNuevo.value == 0xFF38C7 || MemoryLastIf == 2 ){ //RETROCEDER       
+        atras();
+       MemoryLastIf == 2;
+         } 
+      if (codigoLeidoNuevo.value == 0xFF18E7 || MemoryLastIf == 3 ){ //RETROCEDER       
+        parar();
+       MemoryLastIf == 3;
+         } 
+    codigoLeidoNuevo.value=0xFFFFFFFF;
+  }
+
+void adelante()
+{
+  analogWrite(AIA, 255);
+  analogWrite(AIB, 0);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 255);
+  analogWrite(BIB, 0);
+}
+
+ //colocamos en alto los IB y en bajo los IA de los dos motores
+ 
+void atras()
+{
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 255);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 0);
+  analogWrite(BIB, 255);
+  
+}
+
+//Combinamos adelante y atras
+
+void derecha()
+{
+  analogWrite(AIA, 255);
+  analogWrite(AIB, 0);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 0);
+  analogWrite(BIB, 255);
+}
+
+
+//Combinamos adelante y atras pero de forma diferente
+
+void izquierda()
+{
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 255);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 255);
+  analogWrite(BIB, 0);
+  
+}
+
+
+//Colocamos los dos valores en alto
+
+void parar()
+{
+  analogWrite(AIA, 255);
+  analogWrite(AIB, 255);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 255);
+  analogWrite(BIB, 255);
+  delay(50);
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 0);
+  delay(50); //Para evitar pico de consumo
+  analogWrite(BIA, 0);
+  analogWrite(BIB, 0);
+  delay(500);
+}
+//modificar
+void parpadeo(int ledParpadea, int veces, int tiempo){
+    for(int j=0; j<=veces; j++){
+      digitalWrite(ledParpadea, LOW);
+      delay(tiempo); 
+      digitalWrite(ledParpadea, HIGH);
+      delay(tiempo);
+    }
+    }
